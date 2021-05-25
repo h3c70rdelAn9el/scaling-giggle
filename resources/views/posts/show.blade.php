32148808b1283f6711b3351baae8894f3835f1fe
Hector del Angel <hectordelangel@gmail.com>
2021-05-25 11:03:34 -0700

    fix single post page link and some styling

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,48 @@
 @extends('layouts.app')
 
-@section('content')
-  <div class="flex justify-center">
-    <div class="w-10/12 bg-background p-6 rounded-lg">
-      <x-post :post="$post" />
+@section('content')    
+  <h2 class="text-center text-3xl">Post</h2>
+  <div class="post border border-content mt-2 mx-10 rounded-md">
+    <div class="mb-4 p-2">
+      <div class="flex justify-between mb-2">
+        <a href="{{ route('users.posts', $post->user) }}" class="font-bold">
+          {{ $post->user->name }}
+        </a>
+        <span class="text-sm">{{ $post->created_at->toFormattedDateString() }}</span>
+
+      </div>
+      <p class="mb-1">
+        {{ $post->body }}
+      </p>
+
+      {{-- delete post --}}
+      @can('delete', $post)
+        <form action="{{ route('posts.destroy', $post) }}" method="post">
+          @csrf
+          @method('DELETE')
+          <button type="submit" class="text-red-500">Delete</button>
+        </form>
+      @endcan
+
+      {{-- likes --}}
+      <div class="flex mt-2 pt-2 items-center">
+        @auth
+          @if (!$post->likedBy(auth()->user()))
+            <form action="{{ route('posts.likes', $post->id) }}" method="post" class="mr-1">
+              @csrf
+              <button type="submit" class="text-blue-600">Like</button>
+            </form>
+          @else
+            <form action="{{ route('posts.likes', $post->id) }}" method="post" class="mr-1 ml-2">
+              @csrf
+              @method('DELETE')
+              <button type="submit" class="text-blue-600">Unlike</button>
+            </form>
+          @endif
+        @endauth
+
+        <span class="text-sm ml-4">{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
+      </div>
     </div>
   </div>
 @endsection
